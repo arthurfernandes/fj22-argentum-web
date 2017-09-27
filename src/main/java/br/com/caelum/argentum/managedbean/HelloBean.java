fa0ed8f6e8845c805f845681431d305c290bc8d2
lab7011 <lab7011@caelumBSB01-08>
2017-09-27 13:04:36 -0300

    Hello world com Managed Bean

---
+++ src/main/java/br/com/caelum/argentum/managedbean/HelloBean.java
@@ -0,0 +1,10 @@
+package br.com.caelum.argentum.managedbean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class HelloBean {
+	public void teste() {
+		System.out.println("Oi mundo");
+	}
+}
